21ae22d49051036eb03910bba402a552273cb578
CarphatianSnake <sunsn4ke@gmail.com>
2024-06-27 13:55:40 +0300

    fix: change Api path for impirt endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://vwkevd2g6j.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://3zo4a8yu37.execute-api.eu-west-1.amazonaws.com",
+  import: "https://6ddpujuqrb.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
